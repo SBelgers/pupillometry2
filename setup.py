5b6645a7909c922307b729ee44a9f7a3742ef3d0
S. Belgers <s.belgers@tue.nl>
2024-02-21 11:22:46 +0100

    updated requires

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ setup(
     name="pupillometry",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        # add any dependencies your package requires
-    ],
+    install_requires=["numpy", "scipy"],
 )
